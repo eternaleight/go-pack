4ebb5855d84d463d52570fa0376818c0302db185
eternaleight <eternaleight0@gmail.com>
2023-08-14 12:37:57 +0900

    fix(db): Node.jsで作っていたDBのテーブル名、カラム名に統一(Go, Node.js両方から接続可能)

--- models/models.go
+++ models/models.go
@@ -2,14 +2,21 @@ package models
 
 import "time"
 
+// Post model
 type Post struct {
 	ID        int `gorm:"primaryKey"`
 	Content   string
 	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP"`
-	AuthorID  int
+	AuthorID  int       `gorm:"column:authorId"`
 	Author    User
 }
 
+// TableName overrides the table name
+func (Post) TableName() string {
+	return "Post"
+}
+
+// User model
 type User struct {
 	ID       int `gorm:"primaryKey"`
 	Username string
@@ -19,10 +26,21 @@ type User struct {
 	Profile  Profile
 }
 
+// TableName overrides the table name
+func (User) TableName() string {
+	return "User"
+}
+
+// Profile model
 type Profile struct {
 	ID              int    `gorm:"primaryKey"`
 	Bio             string `gorm:"size:1000"`
-	ProfileImageUrl string
-	UserID          int
-	User            *User `gorm:"foreignKey:UserID"`
+	ProfileImageUrl string `gorm:"column:profileImageUrl"`
+	UserID          int    `gorm:"column:userId"`
+	User            *User  `gorm:"foreignKey:UserID"`
+}
+
+// TableName overrides the table name
+func (Profile) TableName() string {
+	return "Profile"
 }
